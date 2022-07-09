84fba976b6f90b6096ed1b123d3b02ae2a9ec140
nx <suleevn@gmail.com>
2022-07-09 19:07:42 +0600

    *

--- src/main/java/swag/rest/education_platform/handler/GlobalAdvice.java
+++ src/main/java/swag/rest/education_platform/handler/GlobalAdvice.java
@@ -1,18 +1,18 @@
-//package swag.rest.education_platform.handler;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//
-//@ControllerAdvice
-//public class GlobalAdvice {
-//    @ResponseBody
-//    @ExceptionHandler
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    StackTraceElement[] studentNotFound(Exception ex) {
-//        return ex.getStackTrace();
-//    }
-//
-//}
+package swag.rest.education_platform.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalAdvice {
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    StackTraceElement[] studentNotFound(Exception ex) {
+        return ex.getStackTrace();
+    }
+
+}
