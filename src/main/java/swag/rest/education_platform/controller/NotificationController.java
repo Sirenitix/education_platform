b72a4d2d56a59d57b1efeb9d9cc10ff60cf961b6
rakhim.lugma <rakhim.lugma@gmail.com>
2022-08-04 15:49:31 +0600

    notifcation for zoom

--- src/main/java/swag/rest/education_platform/controller/NotificationController.java
+++ src/main/java/swag/rest/education_platform/controller/NotificationController.java
@@ -1,16 +1,23 @@
 package swag.rest.education_platform.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import swag.rest.education_platform.entity.ClientNotification;
+import swag.rest.education_platform.service.NotificationService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notification")
+@RequiredArgsConstructor
 public class NotificationController {
+    private final NotificationService service;
 
-    @GetMapping()
-    public ResponseEntity<?> getNotifications() {
-
+    @PostMapping()
+    public List<?> getNotifications(@RequestParam Long user_id, @RequestParam Integer page) {
+        List<ClientNotification> notifications = service.getNotifications(user_id, page);
+        return notifications;
     }
 }
